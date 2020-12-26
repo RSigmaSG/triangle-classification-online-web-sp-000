0a1f28f37136b18b9e5c3bcd89e45d2e4dcb1f37
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-26 10:00:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
 class Triangle
   # write code here
+  
+  attr_accessor :sides
+  
+  def initialize(side_1, side_2, side_3)
+    
+    @sides = [side_1, side_2, side_3]
+    
+  end
+  
+  def kind
+    
+    num_equal = 0
+    
+    @sides.each do |side| 
+      if (side <= 0)
+        begin
+          raise TriangleError
+        rescue TriangleError => error
+            puts error.message
+        end
+      end
+      
+      @sides.collect{|comp_side| compside == side}
+      
+    end
+    
+    case num_equal
+      when 0 
+        return :scalene
+      when 1
+        return :isosceles
+      when 2 
+        return :equilateral
+        
+    end
+      
+    
+  end
+  
+  class TriangleError < StandardError
+    # triangle error code
+
+  end
 end
